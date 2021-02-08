874e44e0a269e360d6f2878cc57df628c3987f75
Santiago <santigp258@gmail.com>
2021-02-08 12:47:24 -0500

    switchMap, tap, and validation into selector

--- src/app/countries/pages/selector-page/selector-page.component.ts
+++ src/app/countries/pages/selector-page/selector-page.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { switchMap, tap } from 'rxjs/operators';
+
 import { CountriesService } from '../../services/countries.service';
 import { CountrySmall } from '../../interfaces/countries.interface';
-
 @Component({
   selector: 'app-selector-page',
   templateUrl: './selector-page.component.html',
@@ -11,7 +12,7 @@ import { CountrySmall } from '../../interfaces/countries.interface';
 export class SelectorPageComponent implements OnInit {
   myForm: FormGroup = this.fb.group({
     region: ['', Validators.required],
-    country: ['', Validators.required]
+    country: ['', Validators.required],
   });
 
   //push selectores
@@ -28,13 +29,29 @@ export class SelectorPageComponent implements OnInit {
 
     //when change region
 
-    this.myForm.get('region')?.valueChanges.subscribe((region) => {
+    /*     this.myForm.get('region')?.valueChanges.subscribe((region) => {
+
       this.countriesService.getCountriesByRegion(region)
       .subscribe(countries=>{
         console.log(countries);
         this.countries = countries;
       });
-    });
+
+    }); */
+
+    this.myForm
+      .get('region')
+      ?.valueChanges
+      .pipe(
+        tap(region =>{
+          this.myForm.get('country')?.reset(''); //clean myform.region
+        }),
+        switchMap(region => this.countriesService.getCountriesByRegion(region))
+        )
+      .subscribe(countries =>{
+        console.log(countries);
+        this.countries = countries;
+      });
   }
 
   submit() {}
