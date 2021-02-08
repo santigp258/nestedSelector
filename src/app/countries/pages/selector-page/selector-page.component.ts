aa897fb365d597b3e0b96e700724de0f5456e6e4
Santiago <santigp258@gmail.com>
2021-02-07 23:56:52 -0500

    lazy load- route children

---
+++ src/app/countries/pages/selector-page/selector-page.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-selector-page',
+  templateUrl: './selector-page.component.html',
+  styles: [
+  ]
+})
+export class SelectorPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
